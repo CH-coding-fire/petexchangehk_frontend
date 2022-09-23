615496c197e1fee571567e957fade1479dabd284
Coding-noob-helpMePlz <67637342+Coding-noob-helpMePlz@users.noreply.github.com>
2022-09-23 22:28:41 +0800

    added response.json

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,8 @@ function App() {
 					// }
 					//If response is ok, then have the response
 					console.log('handling the response...')
-					console.log('the response.status is: ',response.status );
+					console.log('the response.status is: ', response.status);
+					console.log('RESPONSE.JSON:', response.json)
 					if (response.status === 200 || response.status === 204) {
 						console.log('going to pass response.json()')
 						return response.json();
